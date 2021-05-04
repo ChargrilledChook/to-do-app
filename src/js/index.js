eea6380fdd9329537b5d0e1d0651c8ef38603f95
Dylan Williams <happyfrog578+github@gmail.com>
2021-05-04 15:01:16 +1000

    add a basic modal for testing

--- src/js/index.js
+++ src/js/index.js
@@ -16,5 +16,21 @@ main.append(todoHTML.card);
 // Add Todo Button
 const addButton = document.querySelector(".add-todo");
 addButton.addEventListener("click", () => {
-  console.log("Clicky");
+  const modal = document.querySelector('.modal')
+  if(modal.style.display === 'grid') {
+    modal.style.display = 'none'
+  } else {
+    modal.style.display = 'grid'
+  }
 });
+
+const submitButton = document.querySelector("#submit")
+submitButton.addEventListener('click', ()=> {
+  const name = document.querySelector("input[name='name']").value
+  const description = document.querySelector("input[name='description']").value
+  const due =  document.querySelector("input[name='due-date']").value
+
+  const todo = new Todo(name, description, due)
+  const todoCard = new DisplayTodo(todo)
+  main.append(todoCard.card);
+})
