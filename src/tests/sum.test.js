5917cf528916744cae561134df18224fda78b58f
chook <dylanw212@gmail.com>
2021-05-12 09:51:43 +1000

    add comment

--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -1,3 +1,5 @@
+// This was largely a sanity test while setting up testing plugins etc. Can probably be safely deleted now
+
 import { sum } from "../js/sum.js";
 
 test("adds 1 + 2 to equal 3", () => {
